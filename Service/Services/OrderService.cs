f0aaf5408336c4045129ec2fcf564c601be68569
Tiến Hưng <nguyentienhung6967@gmail.com>
2023-04-10 02:01:57 +0700

    update API FollowOrder

--- Service/Services/OrderService.cs
+++ Service/Services/OrderService.cs
@@ -243,7 +243,7 @@ namespace Service.Services
             foreach (var order in orders)
             {
                 OrderJsonFile(order);
-                if (order.StatusId > 1 && order.StatusId < 5) list.Add(order);
+                if (order.StatusId < 4) list.Add(order);
             }
             return list;
         }
