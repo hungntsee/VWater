3d16ac27db209897ddb1c0b35bd35c4f5a69143c
hienle0405200@gmail.com <hienle0405200@gmail.com>
2023-05-20 11:07:11 +0700

    api ReportForStore

---
+++ Controller/Controllers/ReportStoreController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Services;
+
+namespace Controller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportStoreController : ControllerBase
+    {
+        private IReportStoreService _reportStoreService;
+        public ReportStoreController(IReportStoreService reportStoreService)
+        {
+            _reportStoreService = reportStoreService;
+        }
+
+        [HttpGet("/api/GetReportForStoreManager")]
+        public IActionResult GetReportForStoreManager(int store_id)
+        {
+            var number = _reportStoreService.GetReportForNumber(store_id);
+            var revenue = _reportStoreService.GetReportForRevenue(store_id);
+            ICollection<Object> result = new List<Object>();
+
+            result.Add(number);
+            result.Add(revenue);
+
+            return Ok(result);
+        }
+        [HttpGet("/api/GetNumberForDashBoardForStoreManager")]
+        public IActionResult GetNumberForDashboardForStoreManager(int store_id)
+        {
+            var reportStore = _reportStoreService.GetReportForDashboard(store_id);
+            return Ok(reportStore);
+        }
+    }
+}
